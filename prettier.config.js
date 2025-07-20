ae959426f551f98e32d708706742e2afccb7b66c
mainitjerson <mainitjersond@gmail.com>
2025-07-20 18:52:40 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  semi: true,
+  tailwindConfig: "./tailwind.config.js",
+};
